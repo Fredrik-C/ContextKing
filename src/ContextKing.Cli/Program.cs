4869ed33cb45a11d9e552015caaf023fcbddeca8
Fredrik Claesson <fredrik.claesson@mews.com>
2026-04-23 11:13:54 +0200

    bump version to 1.3.9

--- src/ContextKing.Cli/Program.cs
+++ src/ContextKing.Cli/Program.cs
@@ -31,13 +31,13 @@ static void PrintHelp()
 
         Run 'ck <command> --help' for command-specific options.
 
-        Version: 1.3.7
+        Version: 1.3.9
         """);
 }
 
 static int PrintVersion()
 {
-    Console.WriteLine("ck 1.3.7");
+    Console.WriteLine("ck 1.3.9");
     return 0;
 }
 
